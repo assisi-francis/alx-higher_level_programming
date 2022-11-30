56e7bc0f21523a4e449674143d99402aee8682ff
assisi-francis <doncyco123@gmail.com>
2022-11-29 18:55:20 -0800

    add

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,14 @@
 #!/usr/bin/pytho3
 
 def remove_char_at(str, n):
-    if n >= 0:
-        newstr = str[:n] + str[n + 1:]
-        return (newstr)
-    else:
-        return (str)
+    copy = ''
+    if len(str) == 0:
+        return str
+    for i in range(len(str)):
+        if i != n:
+            copy += str[i]
+    return copy
+
+
+if __name__ == '__main__':
+    print(remove_char_at("Holberton School", 3))
