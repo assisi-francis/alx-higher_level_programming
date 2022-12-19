bdc4e96f7cf6f1573e5a2c2baf2490e843040811
assisi-francis <doncyco123@gmail.com>
2022-12-19 13:01:57 -0800

    add new

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
@@ -11,19 +10,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return(total)
-
-if __name__ == '__main__':
-    value = 89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = -89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = "Holberton"
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
