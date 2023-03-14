68316978f49c571e4ca4b07263f339b1f68587d2
assisi-francis <doncyco123@gmail.com>
2023-03-14 01:01:01 +0100

    add tasks 0 -2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cword = 'C is fun';
+const pyword = 'Python is cool';
+const jsWord = 'JavaScript is amazing';
+console.log(cword);
+console.log(pyword);
+console.log(jsWord);
