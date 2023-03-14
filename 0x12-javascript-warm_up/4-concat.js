7a2f391468f7bf4eb33d0d59a5bcd35d4895a964
assisi-francis <doncyco123@gmail.com>
2023-03-14 01:47:08 +0100

    modify 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
 const argsCount = process.argv;
-let length = 0;
-while (argsCount[length] !== undefined) {
-  length++;
-}
-if (length === 2) {
-  console.log(`undefined is undefined`);
-} else if (length === 3) {
-  console.log(`${argsCount[2]} is undefined`);
-} else {
-  console.log(`${argsCount[2]} is ${argsCount[3]}`);
-}
+console.log(`${argsCount[2]} is ${argsCount[3]}`);
