4a98b15ef28a34ce40eaf3f0f49a3dc01af3817a
assisi-francis <doncyco123@gmail.com>
2023-03-15 19:59:25 +0100

    add

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i;
-for (i = 0; i < 3; i++) {
- console.log(words[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
