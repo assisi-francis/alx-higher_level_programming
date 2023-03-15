7d0698d6414ea1b7a8a91100277e8c4424f6003d
assisi-francis <doncyco123@gmail.com>
2023-03-15 20:05:33 +0100

    modify 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-let times = parseInt(process.argv[2]);
-let row = 0;
-let column = 0;
 
-if (isNaN(times)) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (row = 0; row < times; row++) {
-    let s = '';
-    for (column = 0; column < times; column++) {
-      s += 'X';
-    }
-    console.log(s);
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
   }
 }
